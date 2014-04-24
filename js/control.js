d496dcf89bca17982fb54064a3a01f5433a66884
lukasgarcya <lukasgarcya@hotmail.com>
2014-04-24 20:40:48 -0300

    Adequação para exemplo da biblioteca.

--- js/control.js
+++ js/control.js
@@ -1,15 +1,18 @@
 window.onload = function() {
     transition = new Transition();
-    document.getElementById("adicionarLista").addEventListener("click", function(event) {
-        transition.changeRegion("editarLista");
-    });
-    document.getElementById("cancelar").addEventListener("click",function(){
-        transition.changeRegion("mostrarLista");
-    });
-    document.getElementById("mostrarDetalhes").addEventListener("click",function(){
-        transition.changeRegion("detalhes");
+    document.getElementById("mostrarPagina2").addEventListener("click", function(event) {
+        transition.changeRegion("pagina2");
     },false);
-    document.getElementById("voltar").addEventListener("click",function(){
-        transition.changeRegion("editarLista");
+    document.getElementById("voltarPagina1").addEventListener("click",function(){
+        transition.changeRegion("pagina1");
+    },false);
+    document.getElementById("mostrarPagina3").addEventListener("click",function(){
+        transition.changeRegion("pagina3");
+    },false);
+    document.getElementById("voltarPagina2").addEventListener("click",function(){
+        transition.changeRegion("pagina2");
+    },false);
+    document.getElementById("mostrarPagina1").addEventListener("click",function(){
+        transition.changeRegion("pagina1");
     },false);
 };
